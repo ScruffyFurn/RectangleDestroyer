c934f6532e036b56d7ede1486f3c873888dff5d0
Mickey MacDonald <Scruffyfurn@hotmail.com>
2013-04-23 02:03:57 -0400

    Brick bounce issue fixed
    Changed the way the ball reacts when it hits a brick.
    Now the ball will change direction.

--- Rectangle Destroyer/Assets/Scripts/Block.cs
+++ Rectangle Destroyer/Assets/Scripts/Block.cs
@@ -3,8 +3,11 @@ using System.Collections;
 
 public class Block : MonoBehaviour {
 
-	void OnTriggerEnter () {
+	void OnTriggerEnter (Collider other) {
         BreakoutGame.SP.HitBlock();
         Destroy(gameObject);
+		Rigidbody rigid = other.rigidbody;
+        float xDistance = rigid.position.x - transform.position.x;
+        rigid.velocity = new Vector3(rigid.velocity.x + xDistance/2, rigid.velocity.y, -rigid.velocity.z);
 	}
 }
